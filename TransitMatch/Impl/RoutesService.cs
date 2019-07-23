64cec5f0ef2011926f6e4c0bb8a4fbeef98f83ef
Tom Kawchak <thkawcha@microsoft.com>
2019-07-23 12:41:47 -0700

    Finish implementation for RouteService.cs

--- TransitMatch/Impl/RoutesService.cs
+++ TransitMatch/Impl/RoutesService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using AzureMapsToolkit.Common;
 using Microsoft.Azure.KeyVault;
@@ -9,26 +10,38 @@ using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using TransitMatch.Models;
 using TransitMatch.Services;
 
+using System.Linq;
+
 namespace TransitMatch.Impl
 {
     public class RoutesService : IRoutesService
     {
-        private readonly HttpClient client;
+        private readonly HttpClient client = new HttpClient();
         private IKeyVaultClient keyVaultClient;
+        private readonly string AzureMapsSubscriptionKey = "yXcIJq3mB7bC-mdkyuKytBmnHGLHpVK2ewYL0sFJUbQ";
+
+        // How to call Routes Service
+        // var routeService = new RoutesService(new System.Net.Http.HttpClient(), null);
+        // var result = routeService.GetRidesAsync(47.6445277,-122.1391559, 47.60972,-122.3443816, NavigationMode.Rideshare).Result;
+        // foreach (var r in result)
+        // {
+        //     Console.WriteLine($"Length (m):\t{r.Summary.LengthInMeters}\tTime:\t{r.Summary.TravelTimeInSeconds}.");
+        // }
 
         public RoutesService(HttpClient client, IKeyVaultClient keyVaultClient)
         {
-            client = client ?? throw new ArgumentNullException(nameof(client));
+            // this.client = client ?? throw new ArgumentNullException(nameof(client));
             // keyVaultClient = keyVaultClient ?? throw new ArgumentNullException(nameof(client));
         }
 
+
+        // Microsoft Commons: 47.6445277,-122.1391559
+        // Pike Place: 47.60972,-122.3443816
+        // GetRoutes(47.6445277,-122.1391559, 47.60972,-122.3443816, "bus")
         public async Task<List<RouteDirectionsResult>> GetRidesAsync(double lat1, double lon1, double lat2, double lon2, NavigationMode mode)
         {
-            // TODO: Factor in the modes
-            // TODO: Factor in the traffic
-            // TODO: Look for other query params to use
             var routes = new List<RouteDirectionsResult>();
-            var response = await GetRoutes(lat1, lon1, lat2, lon2);
+            var response = await GetRoutesFromApiAsync(lat1, lon1, lat2, lon2, mode);
             var routesResponse = await response.Content.ReadAsAsync<RouteDirectionsResponse>();
             if (routesResponse != null && routesResponse.Routes.Length > 0)
             {
@@ -37,11 +50,30 @@ namespace TransitMatch.Impl
             return routes;
         }
 
-        private async Task<HttpResponseMessage> GetRoutes(double lat1, double lon1, double lat2, double lon2)
+        // Microsoft Commons: 47.6445277,-122.1391559
+        // Pike Place: 47.60972,-122.3443816
+        // GetRoutes(47.6445277,-122.1391559, 47.60972,-122.3443816, "bus", 3)
+        private async Task<HttpResponseMessage> GetRoutesFromApiAsync(double lat1, double lon1, double lat2, double lon2, NavigationMode mode, int numRoutes=3)
         {
-            var queryString = $"https://atlas.microsoft.com/route/directions/json?subscription-key={AzureMapsSubscriptionKey}&api-version=1.0&query={lat1},{lon1}:{lat2},{lon2}";
+            var apiUri = "https://atlas.microsoft.com/route/directions/json";
+            string travelMode = null;
+            // TODO: For now, This is only considering the Drive / Rideshare and Bus (Transit) modes
+            switch (mode)
+            {
+                case NavigationMode.Drive:
+                case NavigationMode.Rideshare:
+                    travelMode = "car";
+                    break;
+                case NavigationMode.Transit:
+                    travelMode="bus";
+                    break;
+                default:
+                    travelMode="car";
+                    break;
+            }
+            var queryString = $"{apiUri}?subscription-key={AzureMapsSubscriptionKey}&api-version=1.0&query={lat1},{lon1}:{lat2},{lon2}&traffic=true&routeType=fastest&maxAlternatives={numRoutes-1}&travelMode={travelMode}";
             Console.WriteLine(queryString);
-            var response = await client.GetAsync(queryString);
+            var response = await this.client.GetAsync(queryString);
             return response;
         }
 
