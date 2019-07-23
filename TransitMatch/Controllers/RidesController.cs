5871543d8b4d7c181f377afbf4308858bb2ca4fc
Tom Kawchak <thkawcha@microsoft.com>
2019-07-23 10:49:27 -0700

    implement api/rides GET Request

--- TransitMatch/Controllers/RidesController.cs
+++ TransitMatch/Controllers/RidesController.cs
@@ -6,6 +6,8 @@ using Microsoft.Azure.KeyVault;
 using Microsoft.Azure.KeyVault.Models;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using AzureMapsToolkit.Common;
+using System.Collections.Generic;
+using TransitMatch.Models;
 
 namespace TransitMatch.Controllers
 {
@@ -13,40 +15,34 @@ namespace TransitMatch.Controllers
     [ApiController]
     public class RidesController : ControllerBase
     {
+        private static HttpClient client = new HttpClient();
         static KeyVaultClient kvc = null;
         static string azureMapsUrl = System.Environment.GetEnvironmentVariable("AZURE_MAPS_URL");
         static Microsoft.Azure.KeyVault.Models.SecretBundle azureMapsSecret = null;
-        private static readonly System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
         private readonly string AzureMapsSubscriptionKey = "yXcIJq3mB7bC-mdkyuKytBmnHGLHpVK2ewYL0sFJUbQ";
 
-        // GET api/values
+        // GET api/rides
+        // https://localhost:5001/api/rides/requestRide?lat1=52.50931&lon1=13.42936&lat2=52.50274&lon2=13.43872
         [HttpGet]
-        public async Task<ActionResult<System.Net.Http.HttpResponseMessage>> Get()
+        public async Task<ActionResult<List<RouteDirectionsResult>>> Get(double lat1, double lon1, double lat2, double lon2, NavigationMode mode)
         {
-            azureMapsSecret = await DoVault();
-            System.Net.Http.HttpResponseMessage response = await client.GetAsync(
-                $"https://atlas.microsoft.com/mapData/upload?subscription-key={AzureMapsSubscriptionKey}&api-version=1.0&dataFormat=geojson"
-                );
-            return response;
+            // TODO: Factor in the modes
+            var routes = new List<RouteDirectionsResult>();
+            var response = await GetRoutes(lat1, lon1, lat2, lon2);
+            var routesResponse = await response.Content.ReadAsAsync<RouteDirectionsResponse>();
+            if (routesResponse != null && routesResponse.Routes.Length > 0)
+            {
+                routes = new List<RouteDirectionsResult>(routesResponse.Routes);
+            }
+            return routes;
         }
 
-        // GET api/rides/rideRequest
-        // &query=52.50931,13.42936:52.50274,13.43872
-        //https://localhost:5001/api/rides/requestRide?lat1=6&lon1=6&lat2=10&lon2=20
-        //52.50931,13.42936:52.50274,13.43872
-        [HttpGet("requestRide")]
-        public async Task<ActionResult<string>> Get(double lat1, double lon1, double lat2, double lon2, int newNum)
+        public async Task<HttpResponseMessage> GetRoutes(double lat1, double lon1, double lat2, double lon2)
         {
-            var num = newNum;
-            var client = new HttpClient();
             var queryString = $"https://atlas.microsoft.com/route/directions/json?subscription-key={AzureMapsSubscriptionKey}&api-version=1.0&query={lat1},{lon1}:{lat2},{lon2}";
             Console.WriteLine(queryString);
             var response = await client.GetAsync(queryString);
-            var responseString = await response.Content.ReadAsStringAsync();
-            var deserializedObject = await response.Content.ReadAsAsync<RouteDirectionsResponse>();
-            Console.WriteLine(deserializedObject);
-            Console.WriteLine(deserializedObject.FormatVersion);
-            return responseString;
+            return response;
         }
 
         public static async Task<string> GetToken(string authority, string resource, string scope)
